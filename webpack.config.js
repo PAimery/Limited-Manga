7585c27c96be1449682b8f8a5d60c0ccea320eb2
Aimery Pineau <aimery.pin@gmail.com>
2023-07-12 23:28:42 +0200

    réalisation de la navbar ainsi qu'une partie du header

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,18 @@ Encore
     .setPublicPath('/build')
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
+    .copyFiles({
+        from: './assets/images',
+
+        // optional target path, relative to the output dir
+        to: 'images/[path][name].[ext]',
+
+        // if versioning is enabled, add the file hash too
+        //to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+    })
 
     /*
      * ENTRY CONFIG
@@ -21,6 +33,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addStyleEntry('home', './assets/styles/home.scss')
+
 
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
