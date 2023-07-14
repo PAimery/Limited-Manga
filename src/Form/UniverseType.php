f1b09975e053335d2eb2b570e76905b56883db6e
Aimery Pineau <aimery.pin@gmail.com>
2023-07-14 18:52:39 +0200

    création du formulaire d'ajout + style

--- src/Form/UniverseType.php
+++ src/Form/UniverseType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Universe;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,13 @@ class UniverseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Title')
-            ->add('image')
-        ;
+            ->add('Title', TextType::class, [
+                'label' => 'Titre',
+                'attr' => [
+                    'placeholder' => 'Titre de l\'univers',
+                ]
+            ])
+            ->add('image');
     }
 
     public function configureOptions(OptionsResolver $resolver): void
